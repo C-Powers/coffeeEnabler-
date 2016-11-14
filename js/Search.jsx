37a6de7b0a873f34b405b126170b966fdb2219a0
CPowers <cjpowers3@gmail.com>
2016-11-14 15:16:33 -0800

    Revert "Geo search"

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,5 +1,4 @@
 const React = require('react')
-const OAuthSimple = require('OAuthSimple')
 const { string } = React.PropTypes
 
 const Search = React.createClass({
@@ -9,36 +8,26 @@ const Search = React.createClass({
   },
   getInitialState () {
     return {
-      position: "It'll take a moment to find where you are... but we will"
+      position: 'unknown'
     }
   },
   componentDidMount () {
     navigator.geolocation.getCurrentPosition(
       (position) => {
-        this.setState({position: position.coords})
-        console.log(position.coords)
+        console.log(position)
+        this.setState({position})
       },
       (error) => console.log(error),
       {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
     )
   },
-  fetchData () {
-    let lat = this.state.position.latitude
-    let lon = this.state.position.longitude
-    let latlon = 'll=' + String(lat) + ',' + String(lon)
-//  TODO: WARNING WARNING ---- DO NOT FORGET TO HIDE THESE IN A CONFIG FILE
-    
-    let dataCatcher = []
-
-  },
-
   render () {
+    const style = {color: this.props.color}
     return (
       <div>
-        <div><pre>{JSON.stringify(this.state.position, null, 2)}</pre></div>
-        <button onClick={this.fetchData} >
-        Press me
-        </button>
+        <h1 style={style}>
+          {this.props.title}
+        </h1>
       </div>
     )
   }
