797990655f95159c07b7175fa281499c16e66e0e
C Powers <cjpowers3@gmail.com>
2016-11-19 18:37:21 -0800

    removed some vistigial scss files. Updated server, and error state

--- server/server.js
+++ server/server.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const _ = require('lodash');
 const morgan = require('morgan');
-const requestYelp = require('../fetchTest.js');
-const fetch = require('isomorphic-fetch');
+//const _ = require('lodash');
+
 
 app.use(morgan('dev'));
 app.use(express.static('public'));
@@ -14,13 +13,6 @@ const defaultParameters = {
   location: 'San+Diego',
   sort: '2'
 }
-
-let yelpData =[]
-
-fetch(requestYelp(defaultParameters))
-  .then(response => response.json())
-  .then(data => console.log('NEW FETCH DATA: -----', data.region))
-  .catch(e => console.log('error: ', e))
 
 app.listen(3000);
 console.log('http://localhost:3000/');
